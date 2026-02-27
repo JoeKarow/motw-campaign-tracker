8d79e41e25f8d8bcb7cc27057bb40bab77d277c1
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2026-02-27 01:43:17 -0500

    fix discord login callback domain
    Signed-off-by: Joe Karow <58997957+JoeKarow@users.noreply.github.com>

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,9 +2,10 @@ import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
 import { admin } from 'better-auth/plugins';
 import { prisma } from '$lib/server/prisma';
-import { DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET } from '$env/static/private';
+import { DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET, BETTER_AUTH_URL } from '$env/static/private';
 
 export const auth = betterAuth({
+	baseURL: BETTER_AUTH_URL,
 	trustedOrigins: ['http://localhost:5173', 'https://motw.jk3.me', 'https://*.discordsays.com'],
 	database: prismaAdapter(prisma, {
 		provider: 'postgresql'
