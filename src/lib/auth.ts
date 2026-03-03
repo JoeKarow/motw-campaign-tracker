1e978f001f3cfe43c4aa3d2b71db123ece26c506
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2026-03-03 13:05:21 -0500

    use dynamic origin
    Signed-off-by: Joe Karow <58997957+JoeKarow@users.noreply.github.com>

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,11 +2,15 @@ import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
 import { admin } from 'better-auth/plugins';
 import { prisma } from '$lib/server/prisma';
-import { DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET, BETTER_AUTH_URL } from '$env/static/private';
+import { DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET } from '$env/static/private';
+
+const authOrigins = ['http://localhost:5173', 'https://motw.jk3.me', 'https://*.discordsays.com'];
 
 export const auth = betterAuth({
-	baseURL: BETTER_AUTH_URL,
-	trustedOrigins: ['http://localhost:5173', 'https://motw.jk3.me', 'https://*.discordsays.com'],
+	baseURL: {
+		allowedHosts: authOrigins
+	},
+	trustedOrigins: authOrigins,
 	database: prismaAdapter(prisma, {
 		provider: 'postgresql'
 	}),
